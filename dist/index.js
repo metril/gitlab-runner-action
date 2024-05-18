b244b7f7ac090961c779a5c75dc9000532721f48
metril <1517921+metril@users.noreply.github.com>
2024-05-18 03:47:06 +0000

    Debug

--- dist/index.js
+++ dist/index.js
@@ -49,7 +49,7 @@ async function unregisterRunnerCmd() {
 async function createRunnerCmd() {
     let cmdArgs = [];
     //cmdArgs.push(`--silent`)
-    cmdArgs.push(`--request`, `POST`, core.getInput('gitlab-url') + `/api/v4/runners`)
+    cmdArgs.push(`--request`, `POST`, `--url`, core.getInput('gitlab-url') + `/api/v4/runners`)
     cmdArgs.push(`--header`,`"PRIVATE-TOKEN: ${core.getInput(`private-token`)}"`)
     cmdArgs.push(`--data`,`runner_type=project_type`)
     cmdArgs.push(`--data`,`project_id="${core.getInput(`project-id`)}"`)
@@ -71,7 +71,7 @@ async function createRunnerCmd() {
 async function deleteRunnerCmd() {
     let cmdArgs = [];
     //cmdArgs.push(`--silent`)
-    cmdArgs.push(`--request`, `DELETE`, core.getInput('gitlab-url') + `/api/v4/runners`)
+    cmdArgs.push(`--request`, `DELETE`, `--url`, core.getInput('gitlab-url') + `/api/v4/runners`)
     cmdArgs.push(`--form`,`token="${global.token}"`)
   
     await exec('curl',cmdArgs);
