daa08b557e85cd5662276e6e0226c1d179ae9c3e
metril <1517921+metril@users.noreply.github.com>
2024-05-18 18:29:31 +0000

    debug

--- dist/index.js
+++ dist/index.js
@@ -73,7 +73,7 @@ async function unregisterRunnerCmd() {
 async function deleteRunnerCmd() {
     let cmdArgs = [];
     cmdArgs.push(`-v`)
-    cmdArgs.push(`--request`, `DELETE`, `--url`, `'${core.getInput('gitlab-url')}/api/v4/runners'`)
+    cmdArgs.push(`--request`, `DELETE`, `--url`, `${core.getInput('gitlab-url')}/api/v4/runners`)
     //cmdArgs.push(`--header`,`"PRIVATE-TOKEN: ${core.getInput(`private-token`)}"`)
     cmdArgs.push(`--data`,`'token=${core.getInput('registration-token')}'`)
   
