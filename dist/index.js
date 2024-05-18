57d588ff97a39a738da95c95a07993463169f504
metril <1517921+metril@users.noreply.github.com>
2024-05-18 05:45:41 +0000

    Changes made

--- dist/index.js
+++ dist/index.js
@@ -46,33 +46,33 @@ async function unregisterRunnerCmd() {
 
 }
 
-async function createRunnerCmd() {
-    let cmdArgs = [];
-    cmdArgs.push(`-v`)
-    cmdArgs.push(`--request`, `POST`, `--url`, `${core.getInput('gitlab-url')}/api/v4/user/runners`)
-    cmdArgs.push(`--data`,`runner_type=project_type`)
-    cmdArgs.push(`--data`,`project_id=${core.getInput(`project-id`)}`)
-    cmdArgs.push(`--data`,`tag_list=${core.getInput(`tag-list`)}`)
-    cmdArgs.push(`--header`,`"PRIVATE-TOKEN: ${core.getInput(`private-token`)}"`)
-    //cmdArgs.push(`--form`,`locked=false`)
-    //cmdArgs.push(`--form`,`access-level="${core.getInput('access-level')}"`)
-    //cmdArgs.push(`--form`,`run-untagged="${core.getInput('run-untagged')}"`)
+// async function createRunnerCmd() {
+//     let cmdArgs = [];
+//     cmdArgs.push(`-v`)
+//     cmdArgs.push(`--request`, `POST`, `--url`, `${core.getInput('gitlab-url')}/api/v4/user/runners`)
+//     cmdArgs.push(`--data`,`runner_type=project_type`)
+//     cmdArgs.push(`--data`,`project_id=${core.getInput(`project-id`)}`)
+//     cmdArgs.push(`--data`,`tag_list=${core.getInput(`tag-list`)}`)
+//     cmdArgs.push(`--header`,`"PRIVATE-TOKEN: ${core.getInput(`private-token`)}"`)
+//     //cmdArgs.push(`--form`,`locked=false`)
+//     //cmdArgs.push(`--form`,`access-level="${core.getInput('access-level')}"`)
+//     //cmdArgs.push(`--form`,`run-untagged="${core.getInput('run-untagged')}"`)
 
-    await exec('curl',cmdArgs);
+//     await exec('curl',cmdArgs);
 
-    console.log(`Curl createRunnerCmd stderr: ${stdout}`)
+//     console.log(`Curl createRunnerCmd stderr: ${stdout}`)
 
-    const response = JSON.parse(stdout)
+//     const response = JSON.parse(stdout)
 
-    global.token = response.token
+//     global.token = response.token
 
-}
+// }
 
 async function deleteRunnerCmd() {
     let cmdArgs = [];
-    //cmdArgs.push(`--silent`)
+    cmdArgs.push(`--silent`)
     cmdArgs.push(`--request`, `DELETE`, `--url`, `${core.getInput('gitlab-url')}/api/v4/runners`)
-    cmdArgs.push(`--form`,`token="${global.token}"`)
+    cmdArgs.push(`--form`,`token=${core.getInput('registration-token')}`)
   
     await exec('curl',cmdArgs);
   }
@@ -104,7 +104,6 @@ async function checkJob(){
 
 async function registerRunner() {
   try{
-    await createRunnerCmd()
     await registerRunnerCmd()
     await startRunnerCmd()
     await checkJob()
