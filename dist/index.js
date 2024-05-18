76abfc481ef1bd304582c04df0fdc88e73be2c1c
metril <1517921+metril@users.noreply.github.com>
2024-05-18 15:37:12 +0000

    Private token added

--- dist/index.js
+++ dist/index.js
@@ -74,6 +74,7 @@ async function deleteRunnerCmd() {
     let cmdArgs = [];
     cmdArgs.push(`-v`)
     cmdArgs.push(`--request`, `DELETE`, `--url`, `${core.getInput('gitlab-url')}/api/v4/runners`)
+    cmdArgs.push(`--header`,`"PRIVATE-TOKEN: ${core.getInput(`private-token`)}"`)
     cmdArgs.push(`--form`,`"token=${core.getInput('registration-token')}"`)
   
     await exec('curl',cmdArgs);
